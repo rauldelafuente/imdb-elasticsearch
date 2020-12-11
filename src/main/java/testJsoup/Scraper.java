b419eafad6a01c6b844ff4f5df55bf2f85fdc1ef
rauldelafuente <raul.delafuentec@gmail.com>
2020-12-11 09:33:02 +0100

    Merged with Bulk_annotation

--- src/main/java/testJsoup/Scraper.java
+++ src/main/java/testJsoup/Scraper.java
@@ -1,18 +1,13 @@
 package testJsoup;
-import java.io.File;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 import com.google.gson.Gson;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -22,24 +17,17 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class Scraper {
 
-	public static ArrayList<Film> filmsList = new ArrayList<Film>();
+	public static StringBuilder finalContent = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
 
 		readExcel();
 
-		Gson gson = new Gson();
-		String filmsJson = gson.toJson(filmsList);
-
-		System.out.println(filmsJson);
-
 		try {
-			FileWriter myWriter = new FileWriter("resultGson.json");
-			myWriter.write(filmsJson);
+			FileWriter myWriter = new FileWriter("resultBulk.json");
+			myWriter.write(finalContent.toString());
 			myWriter.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -56,8 +44,15 @@ public class Scraper {
 		Document doc = Jsoup.connect(url).get();
 		StringBuilder description = new StringBuilder();
 		Elements lst = doc.select("div.summary_text"); //<div clas="summary_text"> </div>
-		for (Element elem:lst) {
-			description.append(elem.text());
+		String fullSum = lst.select("a").attr("abs:href");
+		if(fullSum.equals("") || !fullSum.contains("plotsummary")) {
+			for (Element elem : lst) {
+				description.append(elem.text());
+			}
+		} else {
+			Document docSum = Jsoup.connect(fullSum).get();
+			Elements sum = docSum.select("li.ipl-zebra-list__item");
+			description.append(sum.select("p").get(0).text());
 		}
 		return description.toString();
 	}
@@ -71,7 +66,12 @@ public class Scraper {
 	public static ArrayList<String> getCast(String url) throws IOException {
 		Document doc = Jsoup.connect(url).get();
 		ArrayList<String> cast = new ArrayList<String>();
-		Element table = doc.select("table").get(0); //select the first table.
+		Element table;
+		try {
+			table = doc.select("table").get(0); //select the first table.
+		} catch(IndexOutOfBoundsException e){
+			return cast;
+		}
 		Elements rows = table.select("tr");
 
 		for (int i = 1; i < rows.size(); i++) { //first row is the col names so skip it.
@@ -80,33 +80,32 @@ public class Scraper {
 		    try{
 				cast.add(cols.get(1).select("a").text());
 			} catch (IndexOutOfBoundsException e){
-				//TODO
+				//Skip this line
 			}
 		}
 		return cast;
 	}
-	
-	
-	
+
 	/**
 	 * Este metodo va a leer las cosas del excel
+	 * @throws IOException
+	 * @return
 	 */
-	 private static void readExcel() throws Exception {
+	 private static void readExcel() throws IOException {
 		    XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream("MovieGenreIGC_v3.xlsx"));
 		    XSSFSheet sheet = wb.getSheetAt(0);
 	        
 	        Film film;
 
 	        int rows = sheet.getLastRowNum();
-	        for (int i = 1; i < 10; ++i) {
+	        for (int i = 1; i < rows; ++i) {
+	        	System.out.println(i);
 	        	XSSFRow row = sheet.getRow(i);
 
 	        	XSSFCell imdbIdCell = row.getCell(0);
 	        	XSSFCell linkCell = row.getCell(1);
 	        	XSSFCell titleCell = row.getCell(2);
-	        	//XSSFCell scoreCell = row.getCell(3);
 	        	XSSFCell genreCell = row.getCell(4);
-	        	//XSSFCell posterCell = row.getCell(5);
 
 	            int imdbId = (int) imdbIdCell.getNumericCellValue();
 	            String link = linkCell.getStringCellValue();
@@ -118,42 +117,25 @@ public class Scraper {
 						year = Integer.parseInt(titleAu);
 					}
 				}
-	            //double score = scoreCell.getNumericCellValue();
-	            String[] genreList = genreCell.getStringCellValue().split("\\|");
-	            //String poster = posterCell.getStringCellValue();
+				String[] genreList = new String[0];
+	            try{
+	            	genreList = genreCell.getStringCellValue().split("\\|");
+				} catch (NullPointerException e) {
+
+				}
 	            
 	            String description = getSummary(link);
 	            ArrayList<String> cast = getCast(link);
 	            
 	            film = new Film(imdbId, description, title, year, genreList, cast);
 
-		 		filmsList.add(film);
+				Gson gson = new Gson();
+				String filmOutput = gson.toJson(film);
 
-	            System.out.println(film.toString());
+				finalContent.append("{\"index\":{\"_id\":\"").append(imdbId).append("\"}}\n").
+						append(filmOutput).append("\n");
 
-				/*
-	            ObjectMapper mapper = new ObjectMapper();
-	            
-	            Map<String, Object> filmMap = new HashMap<String, Object>();
-
-	            filmMap.put("imdbid", imdbId);
-		 		filmMap.put("title", title);
-		 		filmMap.put("year", year);
-		 		filmMap.put("genre", genreList);
-		 		filmMap.put("actors", cast);
-		 		filmMap.put("sinopsis", description);
-
-		 		Gson gson = new Gson();
-		 		String filmsJson = gson.toJson(film);
-
-	            try {
-	                mapper.writeValue(new File("resultGson.json"), filmsJson);
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	            **/
-
-	        }
+			}
 	        wb.close();
 	    }
 
